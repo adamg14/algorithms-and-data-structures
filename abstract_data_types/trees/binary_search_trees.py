6edcb1cc1ab92b269eef7fb22d28cd0aa2c9fdca
Adam Worede <adamg2609@gmail.com>
2026-05-09 21:07:28 +0100

    binary search tree traversal

--- abstract_data_types/trees/binary_search_trees.py
+++ abstract_data_types/trees/binary_search_trees.py
@@ -103,15 +103,61 @@ class BinarySearchTree:
         # and returns the array as a binary tree
         bst = BinarySearchTree()
 
-        if len(input_array) == 0:
-            return bst
-        else:
-            map(lambda x: bst.insert(x), input_array)
+        for element in input_array:
+            bst.insert(TreeNode(element))
         
         return bst
-    def deletion(self, node: TreeNode, deletion_value):
+    
+
+    def deletion(self, value_to_delete, node=None):
         # base case
-        pass
+        # hit the bottom of the tree and the parent node
+        # has no children
+        if node is None:
+            return None
+        
+        elif value_to_delete < node.value:
+            node.left_child = self.deletion(value_to_delete, node.left_child)
+
+            # return the current node
+            # to be used as the new value of its parent's left or right child
+            return node
+        
+        elif value_to_delete > node.value:
+            node.right_child = self.deletion(value_to_delete, node.right_child)
+        
+        else:
+            # value_to_delete == node.value
+            
+            # if the current node has no left children
+            # delete it by returning the right child
+            # to be the parent's new sub tree
+            if node.left_child is None:
+                return node.right_child
+            elif node.right_child is None:
+                return node.left_child
+            else:
+                # the current node has two children
+                # delete the node by calling the lift function
+                node.right_child = self.lift(node.right_child, node)
+
+    def lift(self, node, node_to_delete):
+        if node.left_child:
+            # continously go to the left child to 
+            # find the least of the numbers greater than the 
+            # node being deleted (successor node)
+            node.left_child = self.lift(node.left_child, node)
+        
+        # at this point there are no more chains of left children
+        # therefore we have reached our successor node
+        else:
+            node_to_delete.value = node.value
+            return node.right_child
+
+
+        
+
+        
 
 
 bst = BinarySearchTree()
@@ -125,3 +171,7 @@ print(bst.search(8))
 
 print(bst.traverse())
 print(bst.greatest_number())
+
+bst_conversion = BinarySearchTree()
+array_to_bst = bst_conversion.array_bst([5, 3, 7, 2])
+print(array_to_bst)
