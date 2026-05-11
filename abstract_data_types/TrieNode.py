b1ae42354c2124b9a701f58e06302de2c94a100b
Adam Worede <adamg2609@gmail.com>
2026-05-11 22:31:36 +0100

    Trie insertion

--- abstract_data_types/TrieNode.py
+++ abstract_data_types/TrieNode.py
@@ -22,3 +22,20 @@ class Trie:
                 return None
         
         return current_node
+
+
+    def insertion(self, word):
+        current_node = self.root
+
+        for char in word:
+            if current_node.children.get(char):
+                current_node = current_node.children[char]
+            else:
+                # instanciate a new node
+                new_node = TrieNode()
+                current_node.children[char] = new_node
+                current_node = new_node
+        
+        # once the word has been iterated over 
+        # create an aesteriks dictionary entry to show that it is a complete word
+        current_node.children["*"] = None
